0c09cc2682b66dbd3cbd982595e998948321496f
Allan Toelli <allan_tozelli@hotmail.com>
2022-09-20 15:44:38 -0300

    First Commit

---
+++ CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CotacaoDolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Cotacao;
+        public static double ValorEmDolar;
+        public static double IOF = 0.06;
+
+        public static double ValorPago()
+        {
+            return Cotacao * (ValorEmDolar + (ValorEmDolar * IOF));
+        }
+    }
+}
---
+++ CotacaoDolar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.ValorEmDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.ValorPago().ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
